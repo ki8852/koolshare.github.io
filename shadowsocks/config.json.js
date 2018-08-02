245198fd0de21fcda4290efe943acab6a56ca144
ki8852 <40027083+ki8852@users.noreply.github.com>
2018-08-02 12:30:08 +0800

    Update config.json.js

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,6 +1,6 @@
 {
 "version":"3.9.9",
-"md5":"4108455cf5bd256ceef06b9486e425d5",
+"md5":"70a0f371a0fa038d99c4be1dfcbdbd88",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
